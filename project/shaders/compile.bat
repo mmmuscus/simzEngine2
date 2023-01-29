c577a04134315dce7214139bc80dd156e75a7d1d
mmmuscus <mmmuscus@gmail.com>
2023-01-30 00:05:19 +0100

    creating shaders

---
+++ project/shaders/compile.bat
@@ -0,0 +1,3 @@
+C:/devTools/vulkan/Bin/glslc.exe vertexShaders/baseShader.vert -o  vertexShaders/vert.spv
+C:/devTools/vulkan/Bin/glslc.exe fragmentShaders/baseShader.frag -o fragmentShaders/frag.spv
+pause
