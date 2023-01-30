131ce8c0e72305174d1e075914ceb597c3ff26b9
mmmuscus <mmmuscus@gmail.com>
2023-01-31 00:33:03 +0100

    Fixxxxdddd

--- project/vulkanWrappers/src/vkPipelineWrapper.cpp
+++ project/vulkanWrappers/src/vkPipelineWrapper.cpp
@@ -32,6 +32,8 @@ void vkPipelineWrapper::initRenderPass(VkFormat* swapChainImageFormat) {
 	colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
 	colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
 	colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+	colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+	colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
 
 	VkAttachmentReference colorAttachmentRef{};
 	colorAttachmentRef.attachment = 0;
