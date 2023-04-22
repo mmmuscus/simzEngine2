1ab136dad2758781fa9a14867494eb33c4a753f5
mmmuscus <mmmuscus@gmail.com>
2023-04-22 19:16:15 +0200

    imgui adventures :')

--- project/imGuiWrappers/source/imGuiInstanceGui.cpp
+++ project/imGuiWrappers/source/imGuiInstanceGui.cpp
@@ -1,33 +1,28 @@
 #include "../include/imGuiInstance.h"
 
 void imGuiInstance::showGui(scene* currScene) {
-    showObjectGui(currScene->getObjects()[0]);
+    //ImGui::ShowDemoWindow();
+    ImGui::Text("Object settings:");
+    //ImGui::BeginChild("Scrolling");
+    for (size_t i = 0; i < currScene->getObjects().size(); i++) {
+        showObjectGui(currScene->getObjects()[i]);
+    }
+    //ImGui::EndChild();*/
 }
 
 void imGuiInstance::showObjectGui(object* obj) {
-    bool changedPos = false;
-    bool changedRot = false;
-    bool changedScale = false;
+    ImGui::Text("Object %d:", obj->getObjectNumber());
 
     float pos[3] = { obj->getPos().x, obj->getPos().y, obj->getPos().z };
-    changedPos = ImGui::InputFloat3("Position", pos);
-
     float rot[3] = { obj->getRotation().x, obj->getRotation().y, obj->getRotation().z };
-    changedRot = ImGui::SliderFloat3("Rotation", rot, -180.0f, 180.0f);
-
     float scale[3] = { obj->getScale().x, obj->getScale().y, obj->getScale().z };
-    changedScale = ImGui::InputFloat3("Scale", scale);
 
-    if (changedPos) {
-        obj->setPos(glm::vec3(pos[0], pos[1], pos[2]));
-    }
+    ImGui::DragFloat3("Position", pos, 0.1f);
+    ImGui::DragFloat3("Rotation", rot, 0.1f);
+    ImGui::DragFloat3("Scale", scale, 0.1f);
 
-    if (changedRot) {
-        obj->setRotation(glm::vec3(rot[0], rot[1], rot[2]));
-    }
-
-    if (changedScale) {
-        obj->setScale(glm::vec3(scale[0], scale[1], scale[2]));
-    }
+    obj->setPos(glm::vec3(pos[0], pos[1], pos[2]));
+    obj->setRotation(glm::vec3(rot[0], rot[1], rot[2]));
+    obj->setScale(glm::vec3(scale[0], scale[1], scale[2]));
 }
 
