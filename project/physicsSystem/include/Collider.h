b76ab48633a1ec68def04b08128db8360b7db20f
mmmuscus <mmmuscus@gmail.com>
2023-05-01 20:37:07 +0200

    delete uneccesary classes, fields

--- project/physicsSystem/include/Collider.h
+++ project/physicsSystem/include/Collider.h
@@ -12,11 +12,11 @@ class Collider {
 protected:
 	// Collision variables:
 	glm::vec3* pos;
-	glm::vec3* rotation;
+	//glm::vec3* rotation;
 	float extent;
 
 	// Model matrix:
-	glm::mat4 modelMatrix;
+	//glm::mat4 modelMatrix;
 
 public:
 
@@ -24,13 +24,13 @@ public:
 
 	virtual void recalculateExtent() = 0;
 
-	void calculateModelMatrix() {
+	/*void calculateModelMatrix() {
 		modelMatrix = glm::mat4(1.0f);
 		modelMatrix = glm::translate(modelMatrix, *pos);
 		modelMatrix = glm::rotate(modelMatrix, rotation->x, glm::vec3(1.0f, 0.0f, 0.0f));
 		modelMatrix = glm::rotate(modelMatrix, rotation->y, glm::vec3(0.0f, 1.0f, 0.0f));
 		modelMatrix = glm::rotate(modelMatrix, rotation->z, glm::vec3(0.0f, 0.0f, 1.0f));
-	}
+	}*/
 
 private:
 
--- project/physicsSystem/include/SphereCollider.h
+++ project/physicsSystem/include/SphereCollider.h
@@ -8,14 +8,29 @@ private:
 	float radius;
 
 public:
+	SphereCollider(
+		glm::vec3* _pos,
+		float _radius
+	) {
+		pos = _pos;
+		radius = _radius;
+		recalculateExtent();
+	}
 
 	float getRadius() { return radius; }
+
+	void setRadius(float _radius) {
+		radius = _radius;
+		recalculateExtent();
+	}
 
 	void recalculateExtent() { extent = radius; }
 
 	bool doesCollideWith(SphereCollider& other) {
 		if (glm::distance(*pos, *other.getPos()) > radius + other.getRadius())
 			return false;
+
+		printf("Collision!\n");
 
 		return true;
 	}
