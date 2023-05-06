ad4df25c8e360c9ae38721803a9b4f90fb1467ad
mmmuscus <mmmuscus@gmail.com>
2023-05-06 20:03:43 +0200

    capsule capsulle collision works

--- project/physicsSystem/src/CollisionUtilities.cpp
+++ project/physicsSystem/src/CollisionUtilities.cpp
@@ -17,7 +17,7 @@ glm::vec3 Collisions::doCollision(SphereCollider* ego, SphereCollider* other) {
 	glm::vec3 res = glm::normalize(*ego->getPos() - *other->getPos());
 	float scale = ego->getRadius() + other->getRadius() - glm::distance(*ego->getPos(), *other->getPos());
 
-	return res *= scale;
+	return res * scale;
 }
 
 glm::vec3 Collisions::doCollision(SphereCollider* ego, CapsuleCollider* other) {
@@ -32,15 +32,56 @@ glm::vec3 Collisions::doCollision(SphereCollider* ego, CapsuleCollider* other) {
 		*ego->getPos()
 	);
 
-	glm::vec3 res = glm::normalize(*ego->getPos() - closest);
 	float scale = ego->getRadius() + other->getRadius() - glm::distance(*ego->getPos(), closest);
-
 	if (scale < 0.0f)
 		return glm::vec3(0.0f, 0.0f, 0.0f);
 
-	return res *= scale;
+	glm::vec3 res = glm::normalize(*ego->getPos() - closest);
+	return res * scale;
 }
 
+// https://wickedengine.net/2020/04/26/capsule-collision-detection/
 glm::vec3 Collisions::doCollision(CapsuleCollider* ego, CapsuleCollider* other) {
-	return glm::vec3(0.0f, 0.0f, 0.0f);
+	if (doBoundingSpheresCollide(*ego->getPos(), *other->getPos(), ego->getExtent(), other->getExtent()))
+		return glm::vec3(0.0f, 0.0f, 0.0f);
+
+	ego->transformCollider();
+	other->transformCollider();
+
+	// a_A -> ego_Base, a_B -> ego_Tip
+	// b_A -> other_Base, b_B -> other_Tip
+	glm::vec3 v0 = other->getTransformedBase() - ego->getTransformedBase();
+	glm::vec3 v1 = other->getTransformedTip() - ego->getTransformedBase();
+	glm::vec3 v2 = other->getTransformedBase() - ego->getTransformedTip();
+	glm::vec3 v3 = other->getTransformedTip() - ego->getTransformedTip();
+
+	float d0 = glm::dot(v0, v0);
+	float d1 = glm::dot(v1, v1);
+	float d2 = glm::dot(v2, v2);
+	float d3 = glm::dot(v3, v3);
+
+	glm::vec3 closestEgo;
+	if (d2 < d0 || d2 < d1 || d3 < d0 || d3 < d1)
+		closestEgo = ego->getTransformedTip();
+	else
+		closestEgo = ego->getTransformedBase();
+
+	glm::vec3 closestOther = closestPointOnLineSegment(
+		other->getTransformedBase(),
+		other->getTransformedTip(),
+		closestEgo
+	);
+
+	closestEgo = closestPointOnLineSegment(
+		ego->getTransformedBase(),
+		ego->getTransformedTip(),
+		closestOther
+	);
+
+	float scale = ego->getRadius() + other->getRadius() - glm::distance(closestEgo, closestOther);
+	if (scale < 0.0f)
+		return glm::vec3(0.0f, 0.0f, 0.0f);
+
+	glm::vec3 res = glm::normalize(closestEgo - closestOther);
+	return res * scale;
 }
--- project/physicsSystem/src/SphereCollider.cpp
+++ project/physicsSystem/src/SphereCollider.cpp
@@ -3,13 +3,6 @@
 #include "../include/CollisionUtilities.h"
 
 glm::vec3 SphereCollider::doesCollideWith(SphereCollider* other) {
-	/*if (glm::distance(*pos, *other->getPos()) > radius + other->getRadius())
-		return glm::vec3(0.0f, 0.0f, 0.0f);
-
-	glm::vec3 res = glm::normalize(*pos - *other->getPos());
-	float scale = radius + other->getRadius() - glm::distance(*pos, *other->getPos());
-
-	return res *= scale;*/
 	return Collisions::doCollision(this, other);
 }
 
